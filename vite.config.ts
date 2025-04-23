b00a1d427075fcd7580a644d2436440c562eeb4e
dhruv <dhruvranger97@gmail.com>
2025-04-23 22:30:14 +0530

    done

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "Apni-Meal", // Ensure this matches your GitHub repo name
+  base: "./", // Ensure this matches your GitHub repo name
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
